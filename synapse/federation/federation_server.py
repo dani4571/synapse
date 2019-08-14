63179ef52c93f77e9e4d0ded00d64489b5d2cfd9
Jorik Schellekens <joriks@matrix.org>
2019-08-14 17:58:34 +0100

    Stylish imports

--- synapse/federation/federation_server.py
+++ synapse/federation/federation_server.py
@@ -25,7 +25,6 @@ from twisted.internet import defer
 from twisted.internet.abstract import isIPAddress
 from twisted.python import failure
 
-import synapse.logging.opentracing as opentracing
 from synapse.api.constants import EventTypes, Membership
 from synapse.api.errors import (
     AuthError,
@@ -44,6 +43,7 @@ from synapse.federation.persistence import TransactionActions
 from synapse.federation.units import Edu, Transaction
 from synapse.http.endpoint import parse_server_name
 from synapse.logging.context import nested_logging_context
+from synapse.logging.opentracing import start_active_span_from_edu
 from synapse.logging.utils import log_function
 from synapse.replication.http.federation import (
     ReplicationFederationSendEduRestServlet,
@@ -809,7 +809,7 @@ class FederationHandlerRegistry(object):
         if not handler:
             logger.warn("No handler registered for EDU type %s", edu_type)
 
-        with opentracing.start_active_span_from_edu(content, "handle_edu"):
+        with start_active_span_from_edu(content, "handle_edu"):
             try:
                 yield handler(origin, content)
             except SynapseError as e:
